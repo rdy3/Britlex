30e4e272e37fef970e3562b2d6eb4d1f41a483fb
rdy3 <info1aleksandr@mail.ru>
2024-05-13 23:33:23 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+function App() {
+  return (
+    <div>
+      <header className="inline-flex">
+        <h1>Britlex</h1>
+        <nav>
+          <ul className="inline-grid gap-4">
+            <li>
+              <a href="">Home</a>
+            </li>
+
+            <li>
+              <a href="">Skillls</a>
+            </li>
+
+            <li>
+              <a href="">About us</a>
+            </li>
+
+            <li>
+              <a href="">Pricing</a>
+            </li>
+
+            <li>
+              <a href="">Contacts</a>
+            </li>
+          </ul>
+        </nav>
+
+        <button className="rounded-xl">Let's Talk</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
